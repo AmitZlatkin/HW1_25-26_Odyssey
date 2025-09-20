28841ea8f263814dc0b93e5862a6782eb70e3f11
Amit Zlatkin <amit.zlatkin@campus.technion.ac.il>
2025-09-20 21:53:48 +0300

    Fix unsigned int in Utilities.cpp

--- Utilities.cpp
+++ Utilities.cpp
@@ -9,10 +9,10 @@ std::string hash(int key, const std::string& value1, const std::string& value2) 
     for (int i = 0; i < out_size; i++) {
         bytes[i] = key ^ (out_size - i);
     }
-    for (int i = 0; i < value1.size() * out_size ; i++) {
+    for (unsigned int i = 0; i < value1.size() * out_size ; i++) {
         bytes[i % (out_size/2)] ^= value1[i % value1.size()] ^ i;
     }
-    for (int i = 0; i < value2.size() * out_size ; i++) {
+    for (unsigned int i = 0; i < value2.size() * out_size ; i++) {
         bytes[(out_size/2) + (i % out_size/2)] ^= value2[i % value2.size()] ^ i;
     }
     for (int i = 0; i < out_size; i++) {
